94c04dd6f1d29f3bd1df24e653407146c5815ac2
Adrian Richard <adrichard14@hotmail.com>
2025-11-22 17:06:33 -0300

    feature: go RabbitMQ consumer client setup

---
+++ go-consumer/go.mod
@@ -0,0 +1,7 @@
+module golang-rabbitmq-consumer
+
+go 1.25.1
+
+require github.com/streadway/amqp v1.1.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ go-consumer/rabbitmq/rabbitmq.go
@@ -0,0 +1,36 @@
+package rabbitmq
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+var RabbitMQClient *RabbitMQ
+
+type RabbitMQ struct {
+	Conn    *amqp.Connection
+	Channel *amqp.Channel
+}
+
+func NewRabbitMQConnection() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %s", err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a RabbitMQ channel: %s", err)
+	}
+
+	RabbitMQClient = &RabbitMQ{
+		Conn:    conn,
+		Channel: ch,
+	}
+}
+
+func (r *RabbitMQ) CloseConnection() {
+	r.Channel.Close()
+	r.Conn.Close()
+}
